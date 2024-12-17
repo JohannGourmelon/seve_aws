b3d0ebd1b0ec599bfdcc1b00aa951ac4e15bee24
FaustineBrachotte <105442482+FaustineBrachotte@users.noreply.github.com>
2024-12-17 22:50:38 +0100

    add css homepage saas

--- src/main/java/fr/seve/config/WebMvcConfig.java
+++ src/main/java/fr/seve/config/WebMvcConfig.java
@@ -3,16 +3,28 @@ package fr.seve.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
- 
+
 @Configuration
+@EnableWebMvc
 @ComponentScan("fr.seve")
-public class WebMvcConfig {
+public class WebMvcConfig implements WebMvcConfigurer {
+
     @Bean(name = "viewResolver")
     public InternalResourceViewResolver getViewResolver() {
         InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
         viewResolver.setPrefix("/WEB-INF/views/");
         viewResolver.setSuffix(".jsp");
         return viewResolver;
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        // Configuration pour servir les fichiers statiques depuis webapp/resources
+        registry.addResourceHandler("/resources/**")
+                .addResourceLocations("/resources/");
     }
 }
--- src/main/java/fr/seve/controller/SaasController.java
+++ src/main/java/fr/seve/controller/SaasController.java
@@ -4,15 +4,19 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-
 @Controller
 @RequestMapping("/saas")
 public class SaasController {
 
 	@GetMapping
-	public String listAmaps() {
+	public String homeSaas() {
 		
 		return "saas/home";
+	}
+	
+	@GetMapping("/packages")
+	public String packagesSaas() {
+		return "saas/packages";
 	}
 	
 }
