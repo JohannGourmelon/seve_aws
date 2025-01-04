f7fb2af4dd3913e81a8d37763ece20453f424a6a
FaustineBrachotte <105442482+FaustineBrachotte@users.noreply.github.com>
2025-01-04 12:48:05 +0100

    display amap font

--- src/main/java/fr/seve/repository/BoxRepository.java
+++ src/main/java/fr/seve/repository/BoxRepository.java
@@ -3,9 +3,10 @@ package fr.seve.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import fr.seve.entities.AMAP;
 import fr.seve.entities.Box;
 
 @Repository
 public interface BoxRepository extends JpaRepository<Box, Long>{
-
+	
 }
