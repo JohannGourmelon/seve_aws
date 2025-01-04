b9aafd3e1baefd5c3aba13fa580d657142fc4811
FaustineBrachotte <105442482+FaustineBrachotte@users.noreply.github.com>
2025-01-04 21:58:28 +0100

    fixes

--- src/main/java/fr/seve/controller/AmapSpaceController.java
+++ src/main/java/fr/seve/controller/AmapSpaceController.java
@@ -1,12 +1,8 @@
 package fr.seve.controller;
 
-import java.util.List;
-
 import javax.servlet.http.HttpServletRequest;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,13 +11,12 @@ import org.springframework.web.server.ResponseStatusException;
 
 import fr.seve.entities.AMAP;
 
-@Controller
-public class AmapSlugController {
 
-    
+@Controller
+public class AmapSpaceController {
+	
     @GetMapping("/{slug}")
     public String handleAmapSlug(@PathVariable String slug, Model model, HttpServletRequest request) {
-        // Récupérer l'AMAP depuis l'intercepteur
         AMAP amap = (AMAP) request.getAttribute("amap");
         if (amap == null) {
             throw new ResponseStatusException(HttpStatus.NOT_FOUND, "AMAP not found");
@@ -30,6 +25,5 @@ public class AmapSlugController {
         model.addAttribute("amap", amap);
         return "amap-home";
     }
-    
-    
+	
 }
